7529fb7b6512deef039450ddb3bdb70c3c9c67ed
fuqiang <fuqiang@didikon.com>
2019-05-25 21:27:30 +0800

    2-2

---
+++ models/message.js
@@ -0,0 +1,14 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    message_id: String,
+    product_name: String,
+    device_name: String,
+    data_type: String,
+    payload: Buffer
+})
+
+const Message = mongoose.model("Message", messageSchema);
+
+module.exports = Message
